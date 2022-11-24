13111867e08d11904b026eb50eb587615fcc0a80
dhichiafis <ochiengodhiambo56@gmail.com>
2022-11-24 14:36:21 +0300

    the second

--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def hello(request):
     return render(request,'hello/hello.html')
 # Create your views here.
+
+def goobye(request):
+    return render(request,'hello/goodbye.html')
